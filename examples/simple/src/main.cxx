c062d4b5458bafb8dc3ad0e2cfdcde7d75bd5ecf
Markus Rathgeb <maggu2810@gmail.com>
2024-08-25 22:24:34 +0200

    simple main: minor example changes
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- examples/simple/src/main.cxx
+++ examples/simple/src/main.cxx
@@ -1,11 +1,21 @@
-#define LOGGI_FRONTEND 1
+#define LOGGI_FRONTEND 0
 
 #include <loggi/loggi.hxx>
 #include <iostream>
 #include <cstdlib>
 
+
+#if LOGGI_FRONTEND == 0
+#elif LOGGI_FRONTEND == 1
+void pf() {
+    LOGGI_INFO("%s", "hello");
+    LOGGI_WARN("%d %d %d", 1, 2, 3);
+    loggi::warn(LOGGI_SLOC_CONSTR, "%d %d %d", 1, 2, 3);
+}
+#endif
+
 int main(int argc, char *argv[]) {
-    setenv("LOGGI_TARGET", "journal", 1);
+    setenv("LOGGI_TARGET", "stderr", 1);
 
 #if LOGGI_FRONTEND == 0
     LOGGI_INFO("{}", "hello");
@@ -13,10 +23,10 @@ int main(int argc, char *argv[]) {
     loggi::warn(LOGGI_SLOC_CONSTR, "{} {} {}", 1, 2, 3);
     //loggi::warn({}, "{} {} {}", 1, 2, 3);
     //loggi::warn({}, "{} {} {}", 1, 2);
-#else
-    LOGGI_INFO("%s", "hello");
-    LOGGI_WARN("%d %d %d", 1, 2, 3);
-    loggi::warn(LOGGI_SLOC_CONSTR, "%d %d %d", 1, 2, 3);
+#elif LOGGI_FRONTEND == 1
+    for (int i = 0; i < 3000; ++i) {
+        pf();
+    }
 #endif
 
     return 0;
