fb0844ef656c997b2e4c98aae6ee73c29e5d63a4
Markus Rathgeb <maggu2810@gmail.com>
2024-11-21 21:27:20 +0100

    minor ostream format changes
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- src/backend_ostream.hxx
+++ src/backend_ostream.hxx
@@ -7,6 +7,7 @@
 
 #include <loggi/impl/sloc.hxx>
 #include <loggi/level.hxx>
+#include <loggi/impl/fmt.hxx>
 
 #include <string>
 #include <ostream>
@@ -15,11 +16,14 @@
 namespace loggi::backend::ostream {
     inline void log(std::ostream &os, ::loggi::context ctx, ::loggi::level lvl, const std::string &str) {
         const auto &sloc = ctx.sloc();
-        if (!::loggi_impl::sloc_empty(sloc)) {
-            os << "[" << std::filesystem::path(sloc.file_name()).filename().generic_string() << ":" << sloc.line()
-                    << ":" << sloc.column() << "] ";
-        }
-        os << "level: " << static_cast<int>(lvl) << " => " << str << "\n";
+        os << format("[{}{}] {}\n", syslogLevel(lvl), sloc_empty(sloc)
+                                                          ? ""
+                                                          : format(",{}:{}:{}",
+                                                                   std::filesystem::path(sloc.file_name()).
+                                                                   filename().
+                                                                   generic_string(), sloc.line(),
+                                                                   sloc.column()),
+                     str);
     }
 }
 
